6087557f8b9b9b4f454f5eba044bc953ae78c8e6
Samuel <samuelou510@gmail.com>
2024-08-14 17:27:29 -0400

    first commits

---
+++ src/app/api/generate/route.js
@@ -0,0 +1,47 @@
+import Groq from "groq-sdk";
+import { NextResponse } from "next/server";
+
+const groq = new Groq({ apiKey: process.env.NEXT_PUBLIC_GROQ_API_KEY });
+
+const systemPrompt = `
+  You are a flashcard generator designed to help users learn and memorize information effectively. 
+  Your job is to create flashcards based on the text provided by the user. 
+  Each flashcard should contain a concise question on one side and a clear, correct answer on the other. 
+  When generating flashcards, focus on the key concepts, terms, definitions, or questions that are most relevant to the subject matter. 
+  Ensure that the questions are straightforward and the answers are accurate. 
+  If the user provides text that is complex or lengthy, break it down into multiple flashcards for easier learning.
+  Generate 10 flashcards at a time.
+
+  Return in the following JSON format:
+  {
+    "flashcards": [
+      {"front": "string", "back": "string"}
+    ]
+  }
+`;
+
+export async function POST(req) {
+  try {
+    const data = await req.text();
+
+    // Ensure the data is correctly formatted for the API call
+    const response = await groq.chat.completions.create({
+      messages: [
+        { role: "system", content: systemPrompt },
+        { role: "user", content: data },
+      ],
+      model: "llama3-8b-8192",
+      response_format: { type: "json_object" },
+    });
+
+    // Parse the response to extract the flashcards
+    const flashcards = JSON.parse(response.choices[0].message.content);
+    return NextResponse.json(flashcards.flashcards);
+  } catch (error) {
+    console.error("Error generating flashcards:", error);
+    return NextResponse.json(
+      { error: "Failed to generate flashcards" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPcZFgHB2q-rbRn8_YXvLG96exW_j3t_o",
+  authDomain: "flashcards-b05aa.firebaseapp.com",
+  projectId: "flashcards-b05aa",
+  storageBucket: "flashcards-b05aa.appspot.com",
+  messagingSenderId: "988091104462",
+  appId: "1:988091104462:web:0526c7b2548f637874a49f",
+  measurementId: "G-7WFRBFSWNB",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
